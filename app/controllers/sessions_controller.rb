78506a46732323d81795b91be8c09e66bae5409c
Idris Noor <idreesnoor150@gmail.com>
2023-07-01 01:43:57 +0300

    Done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,16 @@
 class SessionsController < ApplicationController
     def create
-      user = User.find_by(username: params[:username])
-      if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        render json: user
-      else
-        render json: { error: 'Invalid username or password' }, status: :unauthorized
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
       end
-    end
-  
-    def destroy
-      reset_session
-      render json: { message: 'Logged out successfully' }
-    end
-  end
-  
+
+      def destroy
+        session.delete :user_id
+        head :no_content
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # Other routes...
-  
-  post '/signup', to: 'users#create'
-  get '/me', to: 'users#show'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  resources :users
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
